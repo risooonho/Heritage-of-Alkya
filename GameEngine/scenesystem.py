96ac70ecd5aa06237eec14f6f78fad89fb5ee298
Lightpearl26 <francklafiteau@gmail.com>
2019-06-25 18:30:10 +0200

    Create BaseScene.py

--- GameEngine/scenesystem.py
+++ GameEngine/scenesystem.py
@@ -10,3 +10,39 @@ import pygame
 
 #§ Variables globales du module
 SCENES = {}
+
+#§ Création des objets du module
+class BaseScene:
+	"""
+	"""
+	def __init__(self):
+		self.surface = pygame.Surface(cts.WINDOW_SIZE, cts.HWSURFACE | cts.SRCALPHA)
+		self.event_dict = {
+			"action": self.action,
+			"cancel": self.cancel,
+			"up": self.up,
+			"down": self.down,
+			"left": self.left,
+			"right": self.right
+		}
+
+	def action(self):
+		pass
+
+	def cancel(self):
+		pass
+
+	def up(self):
+		pass
+
+	def down(self):
+		pass
+
+	def left(self):
+		pass
+
+	def right(self):
+		pass
+
+	def render(self):
+		self.surface.fill((0, 0, 0, 0))
