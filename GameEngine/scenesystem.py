ac2f574cb2998fa1fe43869e14b4f9648e89c8e7
Lightpearl26 <francklafiteau@gmail.com>
2019-06-25 18:19:30 +0200

    Create scenesystem.py

---
+++ GameEngine/scenesystem.py
@@ -0,0 +1,12 @@
+#-*-coding: utf-8-*-
+
+#§ Variables d'information du module
+__version__ = "1.0.0"
+__authors__ = "Lightpearl"
+
+#§ Importation des modules complémentaires nécéssaires
+from . import constants as cts
+import pygame
+
+#§ Variables globales du module
+SCENES = {}
